07e5280e0ecee0583b451e304cd716f8e21e6ae2
Paul Henning <phenning@lanl.gov>
2022-11-07 08:56:06 -0700

    Formatting

--- README.md
+++ README.md
@@ -14,13 +14,14 @@ fluid dynamics simulation packages.
 ## CMake Configuration Options
 
 Package-specific CMake options include:
-* `-DUSE_CATCH2=NO` will disable Ctest testing that relies on version 3 of the Catch2 testing framework.
-* `-DUSE_KOKKOS=YES` will enable Kokkos support, and requires Kokkos to be
-found on the CMake module path.
-* `-DUSE_MPI=YES` will enable MPI support, and requires MPI compilers to
-be available on the search path.
-* `-DUSE_OPENACC=YES` will enable OpenACC support, and requires the C++
-compiler to support it.
+* `-DUSE_CATCH2=NO` will disable Ctest testing that relies on version 3 
+  of the Catch2 testing framework.
+* `-DUSE_KOKKOS=YES` will enable Kokkos support, and requires Kokkos
+  to be found on the CMake module path.
+* `-DUSE_MPI=YES` will enable MPI support, and requires MPI compilers
+  to be available on the search path.
+* `-DUSE_OPENACC=YES` will enable OpenACC support, and requires the
+  C++ compiler to support it.
 
 
 
--- src/Ume/VecN.hh
+++ src/Ume/VecN.hh
@@ -12,11 +12,11 @@
 namespace Ume {
 
 //! Mathematical vector type
-template<class T, size_t N> class VecN {
+template <class T, size_t N> class VecN {
 public:
-  using val = VecN<T,N>;
-  using ref = val&;
-  using const_ref = val const&;
+  using val = VecN<T, N>;
+  using ref = val &;
+  using const_ref = val const &;
 
 public:
   constexpr VecN() {
@@ -33,14 +33,16 @@ public:
     data_.fill(v);
     return *this;
   }
-  constexpr T& operator[](size_t const i) { return data_[i]; }
-  constexpr T const & operator[](size_t const i) const { return data_[i]; }
+  constexpr T &operator[](size_t const i) { return data_[i]; }
+  constexpr T const &operator[](size_t const i) const { return data_[i]; }
   constexpr ref operator+=(const_ref rhs) {
-    for(size_t i = 0; i < N; ++i) data_[i] += rhs.data_[i];
+    for (size_t i = 0; i < N; ++i)
+      data_[i] += rhs.data_[i];
     return *this;
   }
   constexpr ref operator+=(T const &rhs) {
-    for(size_t i = 0; i < N; ++i) data_[i] += rhs;
+    for (size_t i = 0; i < N; ++i)
+      data_[i] += rhs;
     return *this;
   }
   friend val operator+(val lhs, const_ref rhs) {
@@ -52,11 +54,13 @@ public:
     return lhs;
   }
   constexpr ref operator-=(const_ref rhs) {
-    for(size_t i = 0; i < N; ++i) data_[i] -= rhs.data_[i];
+    for (size_t i = 0; i < N; ++i)
+      data_[i] -= rhs.data_[i];
     return *this;
   }
   constexpr ref operator-=(T const &rhs) {
-    for(size_t i = 0; i < N; ++i) data_[i] -= rhs;
+    for (size_t i = 0; i < N; ++i)
+      data_[i] -= rhs;
     return *this;
   }
   friend val operator-(val lhs, const_ref rhs) {
@@ -68,11 +72,13 @@ public:
     return lhs;
   }
   constexpr ref operator*=(const_ref rhs) {
-    for(size_t i = 0; i < N; ++i) data_[i] *= rhs.data_[i];
+    for (size_t i = 0; i < N; ++i)
+      data_[i] *= rhs.data_[i];
     return *this;
   }
   constexpr ref operator*=(T const &rhs) {
-    for(size_t i = 0; i < N; ++i) data_[i] *= rhs;
+    for (size_t i = 0; i < N; ++i)
+      data_[i] *= rhs;
     return *this;
   }
   friend val operator*(val lhs, const_ref rhs) {
@@ -84,11 +90,13 @@ public:
     return lhs;
   }
   constexpr ref operator/=(const_ref rhs) {
-    for(size_t i = 0; i < N; ++i) data_[i] /= rhs.data_[i];
+    for (size_t i = 0; i < N; ++i)
+      data_[i] /= rhs.data_[i];
     return *this;
   }
   constexpr ref operator/=(T const &rhs) {
-    for(size_t i = 0; i < N; ++i) data_[i] /= rhs;
+    for (size_t i = 0; i < N; ++i)
+      data_[i] /= rhs;
     return *this;
   }
   friend val operator/(val lhs, const_ref rhs) {
@@ -113,10 +121,7 @@ private:
   std::array<T, N> data_;
 };
 
-
-
 using Coord3 = VecN<double, 3>;
-
 
 } // namespace Ume
 #endif
