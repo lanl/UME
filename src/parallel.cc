7c9dc3af6b55ab3b37b014da50d07e9c08802dff
Paul Henning <phenning@lanl.gov>
2022-12-23 10:11:11 -0700

    Extend ConnMesh with send/recv elements lists by entity type. Add building and graphing options.

--- src/parallel.cc
+++ src/parallel.cc
@@ -5,9 +5,11 @@
 */
 
 #include "Ume/SOA_Idx_Mesh.hh"
+#include "Ume/Timer.hh"
 #include <cassert>
 #include <fstream>
 #include <iostream>
+#include <iterator>
 #include <map>
 #include <set>
 #include <vector>
@@ -38,6 +40,8 @@ public:
   enum EntType { CORNERS, EDGES, FACES, POINTS, SIDES, ZONES, NUM_ENTITIES };
   size_t numpe, mype;
   Entity const *entities[NUM_ENTITIES];
+  std::vector<int> send_elements[NUM_ENTITIES];
+  std::vector<int> recv_elements[NUM_ENTITIES];
 
 public:
   explicit ConnMesh(Mesh const &m);
@@ -52,6 +56,11 @@ using CONNS = std::vector<ConnMesh>;
 std::vector<Mesh> read_meshes(int const argc, char const *const argv[]);
 MAP1TO1 build_all_recvs(CONNS const &conns, int const entity);
 bool compare_all(MAP1TO1 const &all_recvs, CONNS const &conns, int const ent);
+void build_comm_entities(CONNS &conns, int const entity);
+void graph_1TO1(
+    char const *const filename, MAP1TO1 const &all_recvs, bool const volelem);
+void lookat_subset_ghosts(CONNS const &conns, int const entity);
+/****************************************************************************/
 
 int main(int argc, char const *const argv[]) {
   std::vector<Mesh> ranks{read_meshes(argc, argv)};
@@ -80,7 +89,92 @@ int main(int argc, char const *const argv[]) {
   std::cout << "Zones " << compare_all(all_z_recvs, conn_ranks, ConnMesh::ZONES)
             << '\n';
 
+  Ume::Timer build_timer;
+  build_timer.start();
+  build_comm_entities(conn_ranks, ConnMesh::ZONES);
+  build_timer.stop();
+  std::cout << "build_comm_entities took: " << build_timer << '\n';
+  build_timer.clear();
+  for (auto const &c : conn_ranks) {
+    auto const &csends = c.send_elements[ConnMesh::ZONES];
+    const size_t ns = csends.size();
+    auto const &crecvs = c.recv_elements[ConnMesh::ZONES];
+    const size_t nr = crecvs.size();
+    const size_t nz = c.entities[ConnMesh::ZONES]->size();
+
+    std::cout << "PE" << c.mype << " has (" << nr << "r + " << ns << "s =) "
+              << nr + ns << "/" << nz << " communicating zones" << std::endl;
+    for (auto const &s : c.entities[ConnMesh::ZONES]->subsets) {
+      build_timer.start();
+      std::vector<int> sorted{s.elements};
+      std::sort(sorted.begin(), sorted.end());
+
+      std::vector<int> srecvs;
+      std::set_intersection(crecvs.begin(), crecvs.end(), sorted.begin(),
+          sorted.end(), std::back_inserter(srecvs));
+
+      std::vector<int> ssends;
+      std::set_intersection(csends.begin(), csends.end(), sorted.begin(),
+          sorted.end(), std::back_inserter(ssends));
+      build_timer.stop();
+
+      std::cout << "\t" << s.name << " has " << sorted.size() << " zones, ("
+                << srecvs.size() << "r + " << ssends.size() << "s =) "
+                << srecvs.size() + ssends.size() << " communicating "
+                << build_timer << "\n";
+      build_timer.clear();
+    }
+  }
+
+  lookat_subset_ghosts(conn_ranks, ConnMesh::ZONES);
+  graph_1TO1("z_conn.dot", all_z_recvs, true);
+  graph_1TO1("p_conn.dot", all_p_recvs, false);
   return 0;
+}
+
+void lookat_subset_ghosts(CONNS const &conns, int const entity) {
+  for (auto const &c : conns) {
+    std::cout << "PE" << c.mype << " Subset ghosts" << '\n';
+    for (auto const &s : c.entities[entity]->subsets) {
+      const int elocal = c.entities[entity]->lsize;
+      size_t total = 0, ent_ghost = 0;
+      for (size_t i = s.lsize; i < s.elements.size(); ++i) {
+        total += 1;
+        if (s.elements[i] >= elocal)
+          ent_ghost += 1;
+      }
+      std::cout << '\t' << s.name << ' ' << ent_ghost << '/' << total
+                << " subset ghosts are entity ghosts\n";
+    }
+  }
+}
+
+void graph_1TO1(
+    char const *const filename, MAP1TO1 const &all_recvs, bool const volelem) {
+  std::map<int, std::map<int, size_t>> send_summary;
+  for (auto const &recvr : all_recvs) {
+    int sendpe = recvr.second.pe;
+    int recvpe = recvr.first.pe;
+    auto [it, status] = send_summary[sendpe].insert(std::make_pair(recvpe, 0));
+    it->second += 1;
+  }
+  std::ofstream os(filename);
+  std::string conn;
+  if (volelem) {
+    os << "strict graph {\n";
+    conn = " -- ";
+  } else {
+    os << "digraph {\n";
+    conn = " -> ";
+  }
+
+  for (auto const &s : send_summary) {
+    for (auto const &r : s.second) {
+      os << s.first << conn << r.first << " [ label = " << r.second << "];\n";
+    }
+  }
+  os << "}\n";
+  os.close();
 }
 
 ConnMesh::ConnMesh(Mesh const &m)
@@ -122,6 +216,26 @@ std::vector<Mesh> read_meshes(int const argc, char const *const argv[]) {
   }
 
   return ranks;
+}
+
+void build_comm_entities(CONNS &conns, int const entity) {
+  std::map<int, std::set<int>> sends, recvs;
+  for (auto const &c : conns) {
+    std::vector<int> const &cpy_idx = c.entities[entity]->cpy_idx;
+    std::vector<int> const &src_pe = c.entities[entity]->src_pe;
+    std::vector<int> const &src_idx = c.entities[entity]->src_idx;
+
+    size_t const N = cpy_idx.size();
+    for (size_t i = 0; i < N; ++i) {
+      recvs[c.mype].insert(cpy_idx[i]);
+      sends[src_pe[i]].insert(src_idx[i]);
+    }
+  }
+
+  for (auto &c : conns) {
+    c.send_elements[entity].assign(sends[c.mype].begin(), sends[c.mype].end());
+    c.recv_elements[entity].assign(recvs[c.mype].begin(), recvs[c.mype].end());
+  }
 }
 
 MAP1TO1 build_all_recvs(CONNS const &conns, int const entity) {
