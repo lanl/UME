76e08d5d9232f81324266b0e7931fb80526d886f
Paul Henning <phenning@lanl.gov>
2022-12-27 09:54:36 -0700

    Start adding search/set capabilities

--- src/Ume/Datastore.cc
+++ src/Ume/Datastore.cc
@@ -12,6 +12,51 @@ Datastore::sptr Datastore::add_child_() {
   return children_.back();
 }
 
+std::pair<Datastore::Types, Datastore::sptr> Datastore::find(
+    std::string const &name) {
+  if (int_vars_.count(name))
+    return std::make_pair(Types::INT, getptr());
+  if (vint_vars_.count(name))
+    return std::make_pair(Types::INTV, getptr());
+  if (dbl_vars_.count(name))
+    return std::make_pair(Types::DBL, getptr());
+  if (vdbl_vars_.count(name))
+    return std::make_pair(Types::DBLV, getptr());
+  if (!parent_.expired())
+    return parent_.lock()->find(name);
+  return std::make_pair(Types::NONE, sptr());
+}
+
+void Datastore::set(std::string const &name, int const val) {
+  auto it = int_vars_.find(name);
+  if (it == int_vars_.end()) {
+    int_vars_.insert(std::make_pair(name, val));
+  } else {
+    it->second = val;
+  }
+}
+
+void Datastore::set(std::string const &name, std::vector<int> &val) {
+  auto res = vint_vars_.insert(std::make_pair(name, std::vector<int>()));
+  auto it = res.first;
+  it->second.swap(val);
+}
+
+void Datastore::set(std::string const &name, double const val) {
+  auto it = dbl_vars_.find(name);
+  if (it == dbl_vars_.end()) {
+    dbl_vars_.insert(std::make_pair(name, val));
+  } else {
+    it->second = val;
+  }
+}
+
+void Datastore::set(std::string const &name, std::vector<double> &val) {
+  auto res = vdbl_vars_.insert(std::make_pair(name, std::vector<double>()));
+  auto it = res.first;
+  it->second.swap(val);
+}
+
 Datastore::~Datastore() {
   for (auto &p : children_) {
     p.reset();
--- src/Ume/Datastore.hh
+++ src/Ume/Datastore.hh
@@ -6,6 +6,7 @@
 #define DATASTORE_HH
 
 #include <memory>
+#include <string>
 #include <unordered_map>
 #include <vector>
 
@@ -14,6 +15,7 @@ namespace Ume {
 class Datastore : public std::enable_shared_from_this<Datastore> {
 public:
   using sptr = std::shared_ptr<Datastore>;
+  enum class Types { INT, INTV, DBL, DBLV, NONE };
 
 public:
   sptr getptr() { return shared_from_this(); }
@@ -22,12 +24,25 @@ public:
     return parent->add_child_();
   }
 
+  std::pair<Types, sptr> find(std::string const &name);
+  void set(std::string const &name, int const val);
+  // This destoys val!
+  void set(std::string const &name, std::vector<int> &val);
+  void set(std::string const &name, double const val);
+  // This destoys val!
+  void set(std::string const &name, std::vector<double> &val);
+
+
   ~Datastore();
 
 private:
   // Force the use of the factory function by making the ctors private
   Datastore() = default;
   sptr add_child_();
+  std::unordered_map<std::string, int> int_vars_;
+  std::unordered_map<std::string, std::vector<int>> vint_vars_;
+  std::unordered_map<std::string, double> dbl_vars_;
+  std::unordered_map<std::string, std::vector<double>> vdbl_vars_;
 
 public:
   std::weak_ptr<Datastore> parent_;
