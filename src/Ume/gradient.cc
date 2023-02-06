effc2102077cebf2cf3454dc7044c33713e352c9
Paul Henning <phenning@lanl.gov>
2023-02-06 14:03:09 -0700

    Cleanup

--- src/Ume/gradient.cc
+++ src/Ume/gradient.cc
@@ -10,63 +10,63 @@ using DBLV_T = DS_Types::DBLV_T;
 using VEC3V_T = DS_Types::VEC3V_T;
 using VEC3_T = DS_Types::VEC3_T;
 
-void gradzatp(Ume::SOA_Idx::Mesh &mesh, DBLV_T const &zfield, VEC3V_T &pgrad,
-    DBLV_T &pvol) {
+void gradzatp(Ume::SOA_Idx::Mesh &mesh, DBLV_T const &zone_field,
+    VEC3V_T &point_gradient) {
   auto const &csurf = mesh.ds->caccess_vec3v("corner_csurf");
-  auto const &cv = mesh.ds->caccess_dblv("corner_vol");
-  auto const &pnorm = mesh.ds->caccess_vec3v("point_norm");
-  auto const &kkcp = mesh.ds->caccess_intv("m:c>p");
-  auto const &kkcz = mesh.ds->caccess_intv("m:c>z");
-  auto const &kctyp = mesh.corners.mask;
-  auto const &kptyp = mesh.points.mask;
+  auto const &corner_volume = mesh.ds->caccess_dblv("corner_vol");
+  auto const &point_normal = mesh.ds->caccess_vec3v("point_norm");
+  auto const &c_to_p_map = mesh.ds->caccess_intv("m:c>p");
+  auto const &c_to_z_map = mesh.ds->caccess_intv("m:c>z");
+  auto const &corner_type = mesh.corners.mask;
+  auto const &point_type = mesh.points.mask;
 
   int const pll = mesh.points.size();
   int const pl = mesh.points.lsize;
   int const cl = mesh.corners.lsize;
 
-  pvol.assign(pll, 0.0);
-  pgrad.assign(pll, VEC3_T(0.0));
+  DBLV_T point_volume(pll, 0.0);
+  point_gradient.assign(pll, VEC3_T(0.0));
 
   for (int c = 0; c < cl; ++c) {
-    if (kctyp[c] < 1)
+    if (corner_type[c] < 1)
       continue; // Only operate on interior corners
-    int const z = kkcz[c];
-    int const p = kkcp[c];
-    pvol[p] += cv[c];
-    pgrad[p] += csurf[c] * zfield[z];
+    int const z = c_to_z_map[c];
+    int const p = c_to_p_map[c];
+    point_volume[p] += corner_volume[c];
+    point_gradient[p] += csurf[c] * zone_field[z];
   }
 
-  mesh.points.gathscat(Ume::Comm::Op::SUM, pvol);
-  mesh.points.gathscat(Ume::Comm::Op::SUM, pgrad);
+  mesh.points.gathscat(Ume::Comm::Op::SUM, point_volume);
+  mesh.points.gathscat(Ume::Comm::Op::SUM, point_gradient);
 
   /*
     Divide by point control volume to get gradient.  If a point is on the outer
-    perimeter of the mesh (KPTYP=-1), subtract the outward normal component of
-    the gradient using the point normals (PNORM).
+    perimeter of the mesh (POINT_TYPE=-1), subtract the outward normal component
+    of the gradient using the point normals.
    */
   for (int p = 0; p < pl; ++p) {
-    if (kptyp[p] > 0) {
+    if (point_type[p] > 0) {
       // Internal point
-      pgrad[p] /= pvol[p];
-    } else if (kptyp[p] == -1) {
+      point_gradient[p] /= point_volume[p];
+    } else if (point_type[p] == -1) {
       // Mesh boundary point
-      double const ppdot = dotprod(pgrad[p], pnorm[p]);
-      pgrad[p] = (pgrad[p] - pnorm[p] * ppdot) / pvol[p];
+      double const ppdot = dotprod(point_gradient[p], point_normal[p]);
+      point_gradient[p] =
+          (point_gradient[p] - point_normal[p] * ppdot) / point_volume[p];
     }
   }
 }
 
 void gradzatz(Ume::SOA_Idx::Mesh &mesh, DBLV_T const &zone_field,
-    VEC3V_T &point_gradient, VEC3V_T &zone_gradient) {
+    VEC3V_T &zone_gradient, VEC3V_T &point_gradient) {
   auto const &c_to_z_map = mesh.ds->caccess_intv("m:c>z");
   auto const &c_to_p_map = mesh.ds->caccess_intv("m:c>p");
   int const num_local_corners = mesh.corners.lsize;
   auto const &corner_type = mesh.corners.mask;
   auto const &corner_volume = mesh.ds->caccess_dblv("corner_vol");
 
-  // Get the control volume and the field gradient at each mesh point.
-  DBLV_T point_volume;
-  gradzatp(mesh, zone_field, point_gradient, point_volume);
+  // Get the field gradient at each mesh point.
+  gradzatp(mesh, zone_field, point_gradient);
 
   // Accumulate the zone volume
   DBLV_T zone_volume(mesh.zones.size(), 0.0);
