284e9f06d1dbd0f580b80a5a267e36fb5213e3a6
Paul Henning <phenning@lanl.gov>
2023-02-06 11:42:02 -0700

    Track DSE initialization depth and print initializations on PE0

--- src/Ume/soa_idx_helpers.hh
+++ src/Ume/soa_idx_helpers.hh
@@ -20,12 +20,16 @@
 #define DSE_INIT_PREAMBLE(N) \
   if (init_state_ == Init_State::INITIALIZED) \
     return false; \
-  std::cout << N "::init_()" << std::endl; \
+  init_depth(1); \
+  if (mesh().comm->id() == 0) \
+    std::cout << "PE0: " << std::string(init_depth(0) - 1, '\t') \
+              << N "::init_()" << std::endl; \
   assert(init_state_ != Init_State::IN_PROGRESS); \
   init_state_ = Init_State::IN_PROGRESS
 
 #define DSE_INIT_EPILOGUE \
   init_state_ = Init_State::INITIALIZED; \
+  init_depth(-1); \
   return true;
 
 #endif
